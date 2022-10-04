440a56a171548dbbd9487c588a82de5e5f3f50bc
OPHN\home <josefer.hernandez@hotmail.com>
2022-10-03 21:08:14 -0700

    Se suben los arhivos de vista

---
+++ Patolli_Vista/src/guis/Tablero.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package guis;
+
+/**
+ * 
+ * @author J.Fernando <josefer.hernandez@hotmail.com>
+ */
+public class Tablero {
+
+}
